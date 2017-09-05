93f8a80a50b86ea3b6d5408235a7a8c9ccef25e4
zero <1310937389@qq.com>
2017-09-05 11:35:07 +0800

    Create IDataAccess.py

---
+++ IDataAccess.py
@@ -0,0 +1,10 @@
+class IDataAccessService:
+
+	def select(self, criteria):
+		raise Exception('not support')
+	def update(self, entities):
+		raise Exception('not support')
+	def create(self, entities):
+		raise Exception('not support')
+	def delete(self, entities):
+		raise Exception('not support')
